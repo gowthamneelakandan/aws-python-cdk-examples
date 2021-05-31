785b40fb0856c0ae5b7c690748c3b9d8f57d5c48
gowthamneelakandan <59202376+gowthamneelakandan@users.noreply.github.com>
2021-05-31 18:26:30 +0530

    Update step_function.py

--- CDK-python/step_function.py
+++ CDK-python/step_function.py
@@ -26,80 +26,80 @@ from aws_cdk import (
 import os, boto3, json
 
 class awsstepfunctionservice(core.Stack):
-	## Read local variable from JSON file
-	cleansed_bucket = self.node.try_get_context("cleansed_bucket")
+  ## Read local variable from JSON file
+  cleansed_bucket = self.node.try_get_context("cleansed_bucket")
   
   ## Object for IAM role
-	role_obj = _iam.Role.from_role_arn(self,"stack_name","role_arn", mutable = None)
+  role_obj = _iam.Role.from_role_arn(self,"stack_name","role_arn", mutable = None)
   
   ## Step function with one after another
-	synck1 = _stepfunctions.IntegrationPattern.RUN_JOB
-	gjob_invoke_task = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name1",
-        integration_pattern = synck1
-    )
-    gjob_invoke_task2 = sfn_tasks.GlueStartJobRun(
-        scope=self,
-        "stack_name",
-        glue_job_name="job_name2",
-        integration_pattern = synck1
-    )        
+  synck1 = _stepfunctions.IntegrationPattern.RUN_JOB
+  gjob_invoke_task = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name1",
+      integration_pattern = synck1
+  )
+  gjob_invoke_task2 = sfn_tasks.GlueStartJobRun(
+      scope=self,
+      "stack_name",
+      glue_job_name="job_name2",
+      integration_pattern = synck1
+  )        
 	
-    definition = gjob_invoke_task.next(gjob_invoke_task2)
-    ## Example ##
-    ##        job1
-    ##        job2
+  definition = gjob_invoke_task.next(gjob_invoke_task2)
+  
+  ## Example
+  ##        job1
+  ##        job2
 
-    
-    ## Step function with parallel
-    synck1 = _stepfunctions.IntegrationPattern.RUN_JOB
-    gjob_invoke_task = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name1",
-        integration_pattern = synck1
-    )
-    gjob_invoke_task2 = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name2",
-        integration_pattern = synck1
-    )    
-    gjob_invoke_task3 = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name3",
-        integration_pattern = synck1
-    )
-    gjob_invoke_task4 = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name4",
-        integration_pattern = synck1
-    )
-    gjob_invoke_task5 = sfn_tasks.GlueStartJobRun(
-        self,
-        "stack_name",
-        glue_job_name="job_name5",
-        integration_pattern = synck1
-    )
-    parallel_task = _stepfunctions.Parallel(self, 'Task name')
-    definition = gjob_invoke_task.next(parallel_task
+  ## Step function with parallel
+  synck1 = _stepfunctions.IntegrationPattern.RUN_JOB
+  gjob_invoke_task = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name1",
+      integration_pattern = synck1
+  )
+  gjob_invoke_task2 = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name2",
+      integration_pattern = synck1
+  )    
+  gjob_invoke_task3 = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name3",
+      integration_pattern = synck1
+  )
+  gjob_invoke_task4 = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name4",
+      integration_pattern = synck1
+  )
+  gjob_invoke_task5 = sfn_tasks.GlueStartJobRun(
+      self,
+      "stack_name",
+      glue_job_name="job_name5",
+      integration_pattern = synck1
+  )
+  parallel_task = _stepfunctions.Parallel(self, 'Task name')
+  definition = gjob_invoke_task.next(parallel_task
                                     .branch(gjob_invoke_task2)
                                     .branch(gjob_invoke_task3)
                                     .branch(gjob_invoke_task4)).next(gjob_invoke_task5)
-    ## Example ##
-    ##        job1   
-    ## job2   job3   job4
-    ##        job5
+  ## Example
+  ##        job1   
+  ## job2   job3   job4
+  ##        job5
     
-    ## Create step function
-    stepFunction = _stepfunctions.StateMachine(
-        self,
-        "stack_name",
-        state_machine_name = "step_function_name",
-        definition = definition,
-        role = role_obj ## IAM role obj
-        )
+  ## Create step function
+  stepFunction = _stepfunctions.StateMachine(
+      self,
+      "stack_name",
+      state_machine_name = "step_function_name",
+      definition = definition,
+      role = role_obj ## IAM role obj
+  )
