072964b34aef284b485f5883fda669acdf086991
gowthamneelakandan <59202376+gowthamneelakandan@users.noreply.github.com>
2021-05-31 17:46:34 +0530

    Update glue.py

--- CDK-python/glue.py
+++ CDK-python/glue.py
@@ -38,55 +38,55 @@ class awsglueservice(core.Stack):
   ## By default last folder of your s3 path is the table name created in ATHENA
   ##  else will be added to prefix
   glue_crawler1 = _glue.CfnCrawler(
-            self,
-            'stack_name',
-            name = "crawler_name',
-            role = "role_name",
-            targets = {
-                's3Targets': [
-                    {
-                    'path': "S3-path where your file reside"
-                    }
-                ]
-            },
-            database_name = "database_name",
-            table_prefix = "prefix_" 
-        )
+      self,
+      'stack_name',
+      name = "crawler_name',
+      role = "role_name",
+      targets = {
+          's3Targets': [
+              {
+              'path': "S3-path where your file reside"
+              }
+          ]
+      },
+      database_name = "database_name",
+      table_prefix = "prefix_" 
+      )
   
   ## Create GLUE
   glue_job1 = _glue.CfnJob(
-			self,
-			'stack_name,
-			name = "glue_job_name",
-			role = "role_name",
-			max_capacity = 1,
-			glue_version = "2.0",
-			command = _glue.CfnJob.JobCommandProperty(
-				name='glueetl',
-				python_version = "3",
-				script_location="s3path of script location"
-			),
-			execution_property = _glue.CfnJob.ExecutionPropertyProperty(
-				max_concurrent_runs=100
-			),
-			default_arguments = {
-				'--additional-python-modules': "awswrangler==2.3.0,pyarror==2",
-				'--job_name': "job_name"
-			}
-		)
+      self,
+      'stack_name,
+      name = "glue_job_name",
+      role = "role_name",
+      max_capacity = 1,
+      glue_version = "2.0",
+      command = _glue.CfnJob.JobCommandProperty(
+            name='glueetl',
+            python_version = "3",
+            script_location="s3path of script location"
+      ),
+      execution_property = _glue.CfnJob.ExecutionPropertyProperty(
+            max_concurrent_runs=100
+      ),
+      default_arguments = {
+            '--additional-python-modules': "awswrangler==2.3.0,pyarror==2",
+            '--job_name': "job_name"
+            }
+      )
   
   ## Glue Trigger
   ## schedule in AWS is UTC
   glue_trigger1 = _glue.CfnTrigger(
-            self,
-            "stack_name",
-            schedule = "cron(0 6 * * ? *)", # every day at 8am
-            name="glue_trigger_name",
-            type='SCHEDULED',
-            description= 'Trigger GLUE job, 8AM CET/ 6AM UTC',
-            actions=[
-                _glue.CfnTrigger.ActionProperty(
-                    job_name="glue_job_name")
-                    ],
-            start_on_creation=True
-        )
+      self,
+      "stack_name",
+      schedule = "cron(0 6 * * ? *)", # every day at 8am
+      name="glue_trigger_name",
+      type='SCHEDULED',
+      description= 'Trigger GLUE job, 8AM CET/ 6AM UTC',
+      actions=[
+            _glue.CfnTrigger.ActionProperty(
+		    job_name="glue_job_name")
+      ],
+      start_on_creation=True
+      )
