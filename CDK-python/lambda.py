8b3d8c6c552d34cc78624cad20a4390b487e847b
gowthamneelakandan <59202376+gowthamneelakandan@users.noreply.github.com>
2021-06-01 18:17:29 +0530

    Update lambda.py

--- CDK-python/lambda.py
+++ CDK-python/lambda.py
@@ -23,34 +23,44 @@ from aws_cdk import (
 	aws_events_targets as _targets,
 	aws_secretsmanager as _secman
 )
-
 import os, boto3, json
 
 class awslambdaservice(core.Stack):
   ## Read local variable from JSON file
   cleansed_bucket = self.node.try_get_context("cleansed_bucket")
   ## configure VPC
-  vpc = _ec2.Vpc.from_vpc_attributes( 
-        self, 
-        'stack_name',
-        vpc_id = "vpc_id",
-        availability_zones = core.Fn.get_azs(),
-        private_subnet_ids = ["subnet1", "subnet2"]
+  vpc = _ec2.Vpc.from_vpc_attributes(
+	  self, 
+	  'stack_name',
+	  vpc_id = "vpc_id",
+	  availability_zones = core.Fn.get_azs(),
+	  private_subnet_ids = ["subnet1", "subnet2"]
   )
   ## Create lambda
   event_triggering_lambda = _lambda.Function(
-        self, 
-        'stack_name',
-        runtime = _lambda.Runtime.PYTHON_3_8,
-        vpc = vpc,
-        security_groups=[_ec2.SecurityGroup.from_security_group_id(self, "stack_name", "security_group1")],   
-        code = _lambda.Code.asset('path where the script reside'),
-        timeout=core.Duration.seconds(30),
-        function_name = 'function_name',             
-        role = "role_name",
-        handler='function_name.lambda_handler',
-        environment = {
-        	"parm1" : "parm1_value",
-        	"parm2" : "parm2_value"
-      	} 
+	  self, 
+	  'stack_name',
+	  runtime = _lambda.Runtime.PYTHON_3_8,
+	  vpc = vpc,
+	  security_groups=[_ec2.SecurityGroup.from_security_group_id(self, "stack_name", "security_group1")],   
+	  code = _lambda.Code.asset('path where the script reside'),
+	  timeout=core.Duration.seconds(30),
+	  function_name = 'function_name',
+	  role = "role_name",
+	  handler='function_name.lambda_handler',
+	  environment = {
+		  "parm1" : "parm1_value",
+		  "parm2" : "parm2_value"
+	  }
   )
+  
+  ## create lambda layer
+  lambdaLayer1 = _lambda.LayerVersion(
+	  self,
+	  'stack_name',
+	  code=_lambda.Code.asset('path of zip file'),
+	  compatible_runtimes=[_lambda.Runtime.PYTHON_3_8],
+	  layer_version_name='layer_name',
+	  description='desciption_name'
+  )
+	
